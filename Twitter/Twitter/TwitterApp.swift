006449c7944ee35364e898c6b42984b5c6a2c78a
Jun <eachinabnw168@hotmail.com>
2021-11-19 17:29:21 -0500

    123

---
+++ Twitter/Twitter/TwitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterApp.swift
+//  Twitter
+//
+//  Created by user204085 on 11/19/21.
+//
+
+import SwiftUI
+
+@main
+struct TwitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
