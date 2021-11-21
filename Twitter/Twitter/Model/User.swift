1519aa30f017e92204793030981a38b91c579358
Jun <eachinabnw168@hotmail.com>
2021-11-20 21:52:08 -0500

    123

---
+++ Twitter/Twitter/Model/User.swift
@@ -0,0 +1,35 @@
+//
+//  User.swift
+//  Twitter
+//
+//  Created by user204085 on 11/19/21.
+//
+import Firebase
+
+struct User: Identifiable {
+    let id: String
+    let username: String
+    let profileImageUrl: String
+    let fullname: String
+    let email: String
+    var stats: UserStats
+    var bio: String?
+    
+    var isFollowed = false
+    var isCurrentUser: Bool { return Auth.auth().currentUser?.uid == self.id }
+    
+    init(dictionary: [String: Any]) {
+        self.id = dictionary["uid"] as? String ?? ""
+        self.username = dictionary["username"] as? String ?? ""
+        self.profileImageUrl = dictionary["profileImageUrl"] as? String ?? ""
+        self.email = dictionary["email"] as? String ?? ""
+        self.fullname = dictionary["fullname"] as? String ?? ""
+        self.stats = UserStats(followers: 0, following: 0)
+        self.bio = dictionary["bio"] as? String ?? ""
+    }
+}
+
+struct UserStats {
+    var followers: Int
+    var following: Int
+}
